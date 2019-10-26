72d0bc102200b168e8f566adfe0205fdf7e338f5
Muhammad Umar Farooq <develop.umar@gmail.com>
2019-10-26 15:28:07 +0500

    getting all data from firestore

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import firebase from "./firebase";
+import Spinner from "./components/Spinner";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [showLoader, setShowLoader] = useState(false);
+
+  const getData = async () => {
+    setShowLoader(true);
+    setData([]);
+    const snapshot = await firebase
+      .firestore()
+      .collection("users")
+      .get();
+
+    const dbData = [];
+    snapshot.docs.map(doc => {
+      const object = doc.data();
+      object.id = doc.id;
+      dbData.push(object);
+    });
+    setData(dbData);
+    setShowLoader(false);
+    console.log(dbData);
+  };
+
   return (
     <div>
       <h1>App...</h1>
+      <button onClick={getData}>Get Data</button>
+
+      {data.length > 1 ? (
+        data.map(name => <h1 key={name.name}>{name.name}</h1>)
+      ) : (
+        <div>{showLoader && <Spinner />}</div>
+      )}
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 import "firebase/storage";
@@ -15,5 +15,7 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
+
+// firebase.firestore().settings({ timestampsInSnapshots: true });
 
 export default firebase;
