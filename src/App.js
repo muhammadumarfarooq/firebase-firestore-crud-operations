13da0849c6dd349fc767b86a33120d1dd3d491ee
Muhammad Umar Farooq <develop.umar@gmail.com>
2019-10-26 15:51:28 +0500

    add data to fire store

--- src/App.js
+++ src/App.js
@@ -10,25 +10,44 @@ function App() {
   const getData = async () => {
     setShowLoader(true);
     setData([]);
-    const snapshot = await firebase
-      .firestore()
-      .collection("users")
-      .get();
+    try {
+      const snapshot = await firebase
+        .firestore()
+        .collection("users")
+        .get();
 
-    const dbData = [];
-    snapshot.docs.map(doc => {
-      const object = doc.data();
-      object.id = doc.id;
-      dbData.push(object);
-    });
-    setData(dbData);
-    setShowLoader(false);
-    console.log(dbData);
+      const dbData = [];
+      snapshot.docs.map(doc => {
+        const object = doc.data();
+        object.id = doc.id;
+        dbData.push(object);
+      });
+      setData(dbData);
+      setShowLoader(false);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const addData = async () => {
+    try {
+      const savedData = await firebase
+        .firestore()
+        .collection("users")
+        .add({
+          name: "Nawaz-ud-din",
+          class: "M.A"
+        });
+      getData();
+    } catch (error) {
+      console.error(error.message);
+    }
   };
 
   return (
     <div>
-      <h1>App...</h1>
+      <h1>User Names</h1>
+      <button onClick={addData}>Add Data</button>
       <button onClick={getData}>Get Data</button>
 
       {data.length > 1 ? (
